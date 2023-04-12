04a5a085c4fef904d8ca0b68cbbd44a66093cc04
David Opoku-Ware <99451769+dopok27@users.noreply.github.com>
2023-04-12 04:23:40 +0000

    Need to fixed un-used space on home page and start contact page

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def hello_world():
   return render_template('home.html')
+  
+@app.route("/contact_me")
+def cont_world():
+  return render_template('contact.html')
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
